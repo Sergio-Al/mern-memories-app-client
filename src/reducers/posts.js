d7336e7316b5413daf140cb91102f349d88cf4d1
Sergio-Al <54541807+Sergio-Al@users.noreply.github.com>
2022-01-04 21:01:47 -0400

    Client side Memories App MERN

---
+++ src/reducers/posts.js
@@ -0,0 +1,26 @@
+import {
+  FETCH_ALL,
+  CREATE,
+  UPDATE,
+  DELETE,
+  LIKE,
+} from "../constants/actionTypes";
+
+export default (posts = [], action) => {
+  // remember posts is a state from our funciton (state, action)=>{...}
+  switch (action.type) {
+    case UPDATE:
+    case LIKE:
+      return posts.map((post) =>
+        post._id === action.payload._id ? action.payload : post
+      );
+    case FETCH_ALL:
+      return action.payload;
+    case CREATE:
+      return [...posts, action.payload];
+    case DELETE:
+      return posts.filter((post) => post._id !== action.payload);
+    default:
+      return posts;
+  }
+};
